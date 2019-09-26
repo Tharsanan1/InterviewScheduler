e30fdc8b3e7577c9cb59fc3ccc067c29a2853931
Tharsaanan1 <tharsanan.15@cse.mrt.ac.lk>
2019-09-26 08:31:16 +0530

    max find

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -6,47 +6,36 @@ import java.util.List;
 public class Scheduler {
   private StudentCompany[] studentCompanyPossibilities;
   private ArrayList<Assignment> assignments;
-  private HashMap<StudentCompany, ArrayList<Assignment>> possibleAssignments;
   private int maxRange;
+  private static int maxFind = 0;
 
   public Scheduler(StudentCompany[] studentCompanyPossibilities, int maxRange) {
     this.studentCompanyPossibilities = studentCompanyPossibilities;
     this.assignments = new ArrayList<>();
     this.maxRange = maxRange;
-
-  }
-
-  public void generatePossibleAssignments(){
-    for (int j = 0; j < studentCompanyPossibilities.length; j++) {
-      StudentCompany studentCompany = studentCompanyPossibilities[j];
-      ArrayList<Assignment> assignments = new ArrayList<>();
-      for (int i = 0; i < maxRange + 1; i += studentCompany.getCompany().getTimeUnits()) {
-        TimeRange timeRange = new TimeRange(i, i + studentCompany.getCompany().getTimeUnits());
-        for (int k = 0; k < studentCompany.getCompany().getPanels(); k++) {
-          Assignment assignment = new Assignment(studentCompany, k, timeRange);
-          assignments.add(assignment);
-        }
-      }
-      possibleAssignments.put(studentCompany, assignments);
-    }
   }
 
   public static void main(String[] args) {
 
     // test
 
-    CSVReader csvReader = new CSVReader("/home/tharsanan/Projects/InterviewScheduler/FinalInterviewSchedule.csv");
+    CSVReader csvReader =
+        new CSVReader("/home/tharsanan/Projects/InterviewScheduler/FinalInterviewSchedule.csv");
     try {
       csvReader.readFromFile();
       ArrayList<StudentCompany> studentCompanyArrayList = new ArrayList<>();
       for (int i = 0; i < csvReader.getStudentList().size(); i++) {
         for (int j = 0; j < csvReader.getStudentList().get(i).getCompanies().length; j++) {
-          studentCompanyArrayList.add(new StudentCompany(csvReader.getStudentList().get(i), csvReader.getStudentList().get(i).getCompanies()[j]));
+          studentCompanyArrayList.add(
+              new StudentCompany(
+                  csvReader.getStudentList().get(i),
+                  csvReader.getStudentList().get(i).getCompanies()[j]));
         }
       }
-      Scheduler scheduler = new Scheduler(studentCompanyArrayList.toArray(new StudentCompany[0]), 95);
+      Scheduler scheduler =
+          new Scheduler(studentCompanyArrayList.toArray(new StudentCompany[0]), 96);
       boolean scheduled = scheduler.schedule();
-      if(scheduled){
+      if (scheduled) {
         for (Assignment assignment : scheduler.getAssignments()) {
           System.out.println(
               assignment.getStudentCompany().getStudent().getName()
@@ -57,14 +46,18 @@ public class Scheduler {
                   + " | "
                   + assignment.getTimeRange().toString());
         }
-      }
-      else{
+      } else {
         System.out.println("not found");
       }
     } catch (IOException e) {
       System.out.println("could not read the file");
     }
+  }
 
+  public static void printList(List list) {
+    for (Object o : list) {
+      System.out.println(o.toString());
+    }
   }
 
   public ArrayList<Assignment> getAssignments() {
@@ -72,6 +65,10 @@ public class Scheduler {
   }
 
   public boolean schedule() {
+    if(assignments.size() > maxFind){
+      System.out.println(maxFind);
+      maxFind = assignments.size();
+    }
     StudentCompany studentCompany = getLeastAcceptedStudentCompany();
     if (studentCompany == null) {
       return true;
@@ -105,31 +102,15 @@ public class Scheduler {
               > studentCompanyPossibilities[i].getStudent().getCompanies().length) {
             leastAccepted = studentCompanyPossibilities[i];
           }
-          if(leastAccepted.getStudent().getCompanies().length
-              == studentCompanyPossibilities[i].getStudent().getCompanies().length){
-            if(studentCompanyPossibilities[i].getCompany().getPanels() < leastAccepted.getCompany().getPanels()){
+          if (leastAccepted.getStudent().getCompanies().length
+              == studentCompanyPossibilities[i].getStudent().getCompanies().length) {
+            if (studentCompanyPossibilities[i].getCompany().getPanels()
+                < leastAccepted.getCompany().getPanels()) {
               leastAccepted = studentCompanyPossibilities[i];
             }
           }
         }
       }
-    }
-    return leastAccepted;
-  }
-
-  public StudentCompany getLeastAcceptedStudentCompanyByLeasePossibleAssignments() {
-    StudentCompany leastAccepted = null;
-    for (StudentCompany studentCompany : possibleAssignments.keySet()) {
-      if(!studentCompany.isAssigned()){
-        if(leastAccepted == null){
-          leastAccepted = studentCompany;
-        }
-        else {
-          if(possibleAssignments.get(leastAccepted).size() > possibleAssignments.get(studentCompany).size()){
-            leastAccepted = studentCompany;
-          }
-        }
-        }
     }
     return leastAccepted;
   }
@@ -159,131 +140,28 @@ public class Scheduler {
     }
     return true;
   }
-
-  public void reAddRemovedAssignments(HashMap<StudentCompany, ArrayList<Assignment>> possibleAssignmentsLocal){
-    for (StudentCompany studentCompany : possibleAssignmentsLocal.keySet()) {
-        for (Assignment assignment : possibleAssignmentsLocal.get(studentCompany)) {
-            possibleAssignments.get(studentCompany).add(assignment);
-        }
-    }
-  }
-
-  public Result isValidBasedOnPossibleAssignmentLeft(Assignment assignment) {
-    for (Assignment assignmentLocal : assignments) {
-      if (assignment
-          .getStudentCompany()
-          .getCompany()
-          .getName()
-          .equals(assignmentLocal.getStudentCompany().getCompany().getName())) {
-        if (assignment.getPanelNumber() == assignmentLocal.getPanelNumber()) {
-          if (TimeRange.clashes(assignment.getTimeRange(), assignmentLocal.getTimeRange())) {
-            return new Result(null, false);
-          }
-        }
-      }
-      if (assignment
-          .getStudentCompany()
-          .getStudent()
-          .getName()
-          .equals(assignmentLocal.getStudentCompany().getStudent().getName())) {
-        if (TimeRange.clashesStudent(assignment.getTimeRange(), assignmentLocal.getTimeRange())) {
-          return new Result(null, false);
-        }
-      }
-    }
-    HashMap<StudentCompany, ArrayList<Assignment>> possibleAssignmentsLocal = new HashMap<>();
-    for (StudentCompany studentCompany : possibleAssignments.keySet()) {
-      possibleAssignmentsLocal.put(studentCompany, new ArrayList<Assignment>());
-      if (!studentCompany.isAssigned()) {
-        for (Assignment assignment1 : possibleAssignments.get(studentCompany)) {
-          if (assignment
-              .getStudentCompany()
-              .getCompany()
-              .getName()
-              .equals(assignment1.getStudentCompany().getCompany().getName())) {
-            if (assignment.getPanelNumber() == assignment1.getPanelNumber()) {
-              if (TimeRange.clashes(assignment.getTimeRange(), assignment1.getTimeRange())) {
-                possibleAssignments.get(studentCompany).remove(assignment1);
-                possibleAssignmentsLocal.get(studentCompany).add(assignment1);
-                if(possibleAssignments.get(studentCompany).size() == 0){
-                  return new Result(possibleAssignmentsLocal, false);
-                }
-              }
-            }
-          }
-          if (assignment
-              .getStudentCompany()
-              .getStudent()
-              .getName()
-              .equals(assignment1.getStudentCompany().getStudent().getName())) {
-            if (TimeRange.clashesStudent(assignment.getTimeRange(), assignment1.getTimeRange())) {
-              possibleAssignments.get(studentCompany).remove(assignment1);
-              possibleAssignmentsLocal.get(studentCompany).add(assignment1);
-              if(possibleAssignments.get(studentCompany).size() == 0){
-                return new Result(possibleAssignmentsLocal, false);
-              }
-            }
-          }
-        }
-      }
-    }
-    return new Result(possibleAssignmentsLocal, false);
-  }
-
-  public static void printList(List list){
-    for (Object o : list) {
-        System.out.println(o.toString());
-    }
-  }
-}
-class Result{
-  HashMap<StudentCompany, ArrayList<Assignment>> possibleAssignments;
-  boolean isValid;
-
-  public Result(HashMap<StudentCompany, ArrayList<Assignment>> possibleAssignments, boolean isValid) {
-    this.possibleAssignments = possibleAssignments;
-    this.isValid = isValid;
-  }
 }
 
 /**
- Company sys = new Company("sys", 6, 3);
- Company wso = new Company("wso", 6, 2);
- Company enac = new Company("ena", 3, 1);
-
- Student thar = new Student("Tha", new Company[] {sys, wso, enac});
- Student kir = new Student("kir", new Company[] {sys, wso, enac});
- Student chru = new Student("chr", new Company[] {sys, wso, enac});
- Student bra = new Student("bra", new Company[] {sys, wso, enac});
- Student sar = new Student("sar", new Company[] {sys, wso, enac});
-
- ArrayList<StudentCompany> studentCompaniesList = new ArrayList<>();
- for (Student student : new Student[] {thar, kir, chru, bra, sar}) {
- for (Company company : student.getCompanies()) {
- studentCompaniesList.add(new StudentCompany(student, company));
- }
- }
- StudentCompany[] studentCompanies = new StudentCompany[studentCompaniesList.size()];
-
- for (int i = 0; i < studentCompaniesList.size(); i++) {
- studentCompanies[i] = studentCompaniesList.get(i);
- }
-
- Scheduler scheduler = new Scheduler(studentCompanies, 20);
- if (scheduler.schedule()) {
- for (Assignment assignment : scheduler.getAssignments()) {
- System.out.println(
- assignment.getStudentCompany().getStudent().getName()
- + " | "
- + assignment.getStudentCompany().getCompany().getName()
- + " | "
- + assignment.getPanelNumber()
- + " | "
- + assignment.getTimeRange().toString());
- }
- }
- else{
- System.out.println("not found");
- }
+ * Company sys = new Company("sys", 6, 3); Company wso = new Company("wso", 6, 2); Company enac =
+ * new Company("ena", 3, 1);
  *
- **/
+ * <p>Student thar = new Student("Tha", new Company[] {sys, wso, enac}); Student kir = new
+ * Student("kir", new Company[] {sys, wso, enac}); Student chru = new Student("chr", new Company[]
+ * {sys, wso, enac}); Student bra = new Student("bra", new Company[] {sys, wso, enac}); Student sar
+ * = new Student("sar", new Company[] {sys, wso, enac});
+ *
+ * <p>ArrayList<StudentCompany> studentCompaniesList = new ArrayList<>(); for (Student student : new
+ * Student[] {thar, kir, chru, bra, sar}) { for (Company company : student.getCompanies()) {
+ * studentCompaniesList.add(new StudentCompany(student, company)); } } StudentCompany[]
+ * studentCompanies = new StudentCompany[studentCompaniesList.size()];
+ *
+ * <p>for (int i = 0; i < studentCompaniesList.size(); i++) { studentCompanies[i] =
+ * studentCompaniesList.get(i); }
+ *
+ * <p>Scheduler scheduler = new Scheduler(studentCompanies, 20); if (scheduler.schedule()) { for
+ * (Assignment assignment : scheduler.getAssignments()) { System.out.println(
+ * assignment.getStudentCompany().getStudent().getName() + " | " +
+ * assignment.getStudentCompany().getCompany().getName() + " | " + assignment.getPanelNumber() + " |
+ * " + assignment.getTimeRange().toString()); } } else{ System.out.println("not found"); }
+ */
